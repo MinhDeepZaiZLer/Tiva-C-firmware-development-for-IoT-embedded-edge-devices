121ac63f36a069490eb07e9478d5d768d66bd9a1
MinhDeepZaiZLer <nguyengiaminh101105@gmail.com>
2026-07-08 16:00:34 +0700

    feat: add UART0 module, add WriteString to state.c | fix: fix switch press event

---
+++ Debug/adc.d
@@ -0,0 +1,6 @@
+# FIXED
+
+adc.o: ../adc.c ../adc.h \
+ C:/ti/TivaWare_C_Series-2.2.0.295/inc/tm4c123gh6pm.h
+../adc.h:
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/tm4c123gh6pm.h:
--- Debug/subdir_vars.mk
+++ Debug/subdir_vars.mk
@@ -9,44 +9,56 @@ CMD_SRCS += \
 ../project0_ccs.cmd 
 
 C_SRCS += \
+../adc.c \
 ../gpio.c \
 ../main.c \
 ../startup_ccs.c \
 ../state.c \
-../uart.c 
+../uart.c \
+../uart1.c 
 
 C_DEPS += \
+./adc.d \
 ./gpio.d \
 ./main.d \
 ./startup_ccs.d \
 ./state.d \
-./uart.d 
+./uart.d \
+./uart1.d 
 
 OBJS += \
+./adc.o \
 ./gpio.o \
 ./main.o \
 ./startup_ccs.o \
 ./state.o \
-./uart.o 
+./uart.o \
+./uart1.o 
 
 OBJS__QUOTED += \
+"adc.o" \
 "gpio.o" \
 "main.o" \
 "startup_ccs.o" \
 "state.o" \
-"uart.o" 
+"uart.o" \
+"uart1.o" 
 
 C_DEPS__QUOTED += \
+"adc.d" \
 "gpio.d" \
 "main.d" \
 "startup_ccs.d" \
 "state.d" \
-"uart.d" 
+"uart.d" \
+"uart1.d" 
 
 C_SRCS__QUOTED += \
+"../adc.c" \
 "../gpio.c" \
 "../startup_ccs.c" \
 "../state.c" \
-"../uart.c" 
+"../uart.c" \
+"../uart1.c" 
 
 
