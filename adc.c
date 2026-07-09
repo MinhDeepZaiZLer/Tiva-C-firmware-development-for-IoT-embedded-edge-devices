3c3e2bb89d5c1bb5ff30b842b28ea5fd6bf4361e
MinhDeepZaiZLer <nguyengiaminh101105@gmail.com>
2026-07-09 17:07:11 +0700

    feat: add ADC feature || fix : WriteChar/ WriteString at while loop in main || fix: remove sprintf, dont use debug_buffer in loop

--- adc.c
+++ adc.c
@@ -5,14 +5,19 @@ void ADC0_Init(void) {
 
   // enable clock for ADC0 and GPIO port E
   SYSCTL_RCGCADC_R |= (1 << 0);
-  SYSCTL_RCGCGPIO_R |= (1 << 1); // 0x10 = 4 => Port E
+  SYSCTL_RCGCGPIO_R |= (1 << 4); // 0x10 = 4 => Port E
   // wait until ready
 
   while ((SYSCTL_RCGCADC_R & (1 << 0)) == 0)
     ;
 
-  while ((SYSCTL_RCGCGPIO_R & (1 << 2)) == 0)
+  while ((SYSCTL_RCGCGPIO_R & (1 << 4)) == 0)
     ;
+
+  GPIO_PORTE_AFSEL_R |= (1 << 3); // Bật chức năng Alternate function cho PE3
+  GPIO_PORTE_DEN_R &= ~(1 << 3);  // Tắt chức năng kỹ thuật số (Digital) tại PE3
+  GPIO_PORTE_AMSEL_R |= (1 << 3); // Bật chức năng Analog trên PE3
+  
   // Config PE3 as Analog output (AIN0)
 
   ADC0_ACTSS_R &= ~(1 << 3); // disable SS3 before config
@@ -22,15 +27,25 @@ void ADC0_Init(void) {
   // Configure sample control:
   // END0 = 1 : This is the last sample in the sequence
   // IE0  = 1 : Generate interrupt flag when conversion completes
-  ADC0_SSCTL3_R = 0x06;
+  ADC0_SSCTL3_R = 0x06; // 0110
 
   // Enable Sample Sequencer 3
-  ADC0_ACTSS_R |= 0x08;
+  ADC0_ACTSS_R |= (1 << 3);
 }
 
 uint32_t ADC0_Read(void) {
-    uint32_t result;
+  uint32_t result;
+  // 1. Initiate software trigger for SS3
+  ADC0_PSSI_R = (1 << 3);
 
-    
-    return  result;
+  // 2. Wait for conversion to complete (polling the raw interrupt status flag)
+  while ((ADC0_RIS_R & (1 << 3)) == 0)
+    ;
+
+  // 3. Read raw 12-bit conversion result from FIFO
+  result = ADC0_SSFIFO3_R & 0xFFF;
+
+  // 4. Clear the interrupt flag to prepare for the next conversion
+  ADC0_ISC_R = (1 << 3);
+  return result;
 }
