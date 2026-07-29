67a9fa27056b13d36d38176b781008e292766324
MinhDeepZaiZLer <nguyengiaminh101105@gmail.com>
2026-07-29 15:08:09 +0700

    feat: UART1 for AT interface to Wifi Scan and Connect

--- Common/delay.h
+++ Common/delay.h
@@ -1,12 +1,14 @@
 #ifndef DELAY_H_
 #define DELAY_H_
-#include <stdbool.h>
-#include "driverlib/sysctl.h"
-#include <stdint.h>
 
-// Ở clock 16MHz (SYSCTL_SYSDIV_1), 1ms tương đương khoảng 5333 chu kỳ của SysCtlDelay
+#include <stdint.h>
+#include "driverlib/sysctl.h"
+
 static inline void Delay_ms(uint32_t ms) {
-    SysCtlDelay(ms * 5333);
+    uint32_t ticks_per_ms = SysCtlClockGet() / 3000u;
+    while (ms--) {
+        SysCtlDelay(ticks_per_ms);
+    }
 }
 
 #endif
--- Debug/Drivers/UART/subdir_vars.mk
+++ Debug/Drivers/UART/subdir_vars.mk
@@ -6,21 +6,27 @@ SHELL = cmd.exe
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Drivers/UART/uart.c 
+../Drivers/UART/uart.c \
+../Drivers/UART/uart1.c 
 
 C_DEPS += \
-./Drivers/UART/uart.d 
+./Drivers/UART/uart.d \
+./Drivers/UART/uart1.d 
 
 OBJS += \
-./Drivers/UART/uart.o 
+./Drivers/UART/uart.o \
+./Drivers/UART/uart1.o 
 
 OBJS__QUOTED += \
-"Drivers\UART\uart.o" 
+"Drivers\UART\uart.o" \
+"Drivers\UART\uart1.o" 
 
 C_DEPS__QUOTED += \
-"Drivers\UART\uart.d" 
+"Drivers\UART\uart.d" \
+"Drivers\UART\uart1.d" 
 
 C_SRCS__QUOTED += \
-"../Drivers/UART/uart.c" 
+"../Drivers/UART/uart.c" \
+"../Drivers/UART/uart1.c" 
 
 
